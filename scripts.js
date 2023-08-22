bf8827559470a522ad18de23dccde492548acdf4
spalme21 <spalme21@gmail.com>
2023-08-22 11:39:22 -0500

    Add functional remove button

--- scripts.js
+++ scripts.js
@@ -22,9 +22,10 @@ addBookToLibrary("Catch-22", "Joseph Heller", 300);
 function display() {
     const bookshelf = document.querySelector(".bookshelf");
     bookshelf.replaceChildren();
-    for (book of myLibrary) {
+    myLibrary.forEach ((book, index) => {
         const newBook = document.createElement("div");
         newBook.classList.add("book");
+        newBook.setAttribute("id", index);
         const title = document.createElement("h2");
         title.textContent = book.title;
         const author = document.createElement("p");
@@ -33,13 +34,27 @@ function display() {
         pages.textContent = book.pages;
         const read = document.createElement("p");
         read.textContent = book.read ? "Read" : "Not Read";
+        const readButton = document.createElement("button");
+        readButton.textContent = "Mark as Read";
+        readButton.classList.add("read");
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.classList.add("remove");
         newBook.appendChild(title);
         newBook.appendChild(author);
         newBook.appendChild(pages);
         newBook.appendChild(read);
+        newBook.appendChild(readButton);
+        newBook.appendChild(removeButton);
         bookshelf.appendChild(newBook);
-    }
-}
+    });
+    const removeBtns = document.querySelectorAll("button.remove");
+    removeBtns.forEach(btn => {
+        console.log(btn);
+        btn.addEventListener('click', removeBook);
+    });
+
+};
 
 display();
 
@@ -50,10 +65,11 @@ const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 
+
 newBtn.addEventListener("click", () => {
     title.value = "";
     author.value = "";
-    pages.value = 0;
+    pages.value = "";
     dialog.showModal();
 });
 
@@ -63,3 +79,12 @@ addBtn.addEventListener("click", (e) => {
     display();
     dialog.close();
 });
+
+function removeBook(e) {
+    console.log(e);
+    const index = e.target.parentElement.id;
+    myLibrary.splice(index, 1);
+    display();
+};
+
+
